dbf845c3bc2bbed629bd00fc07060defa89a6569
Lkianjo2 <Lkianjo2@dso.org.sg>
2020-05-13 00:34:56 +0800

    Update crosscompile.cmake
    Make the compilers path independent of QNX-specific environment variables.

--- build/arm-qnx/crosscompile.cmake
+++ build/arm-qnx/crosscompile.cmake
@@ -14,20 +14,20 @@ set(CMAKE_SYSTEM_PROCESSOR ARMV7L)
 #set(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)
 
 # specify the cross compiler
-set(CMAKE_C_COMPILER $ENV{QNX_HOST}/usr/bin/arm-unknown-nto-qnx7.0.0eabi-gcc)
+set(CMAKE_C_COMPILER $ENV{HOME}/qnx700/host/linux/x86_64/usr/bin/arm-unknown-nto-qnx7.0.0eabi-gcc)
 
-set(CMAKE_CXX_COMPILER $ENV{QNX_HOST}/usr/bin/arm-unknown-nto-qnx7.0.0eabi-g++)
+set(CMAKE_CXX_COMPILER $ENV{HOME}/qnx700/host/linux/x86_64/usr/bin/arm-unknown-nto-qnx7.0.0eabi-g++)
 
-set(CMAKE_ASM_COMPILER $ENV{QNX_HOST}/usr/bin/arm-unknown-nto-qnx7.0.0eabi-as)
+set(CMAKE_ASM_COMPILER $ENV{HOME}/qnx700/host/linux/x86_64/usr/bin/arm-unknown-nto-qnx7.0.0eabi-as)
 set(CMAKE_ASM_DEFINE_FLAG "-Wa,--defsym,")
 
-set(CMAKE_RANLIB $ENV{QNX_HOST}/usr/bin/arm-unknown-nto-qnx7.0.0eabi-ranlib
+set(CMAKE_RANLIB $ENV{HOME}/qnx700/host/linux/x86_64/usr/bin/arm-unknown-nto-qnx7.0.0eabi-ranlib
     CACHE PATH "QNX ranlib Program" FORCE)
-set(CMAKE_AR $ENV{QNX_HOST}/usr/bin/arm-unknown-nto-qnx7.0.0eabi-ar
+set(CMAKE_AR $ENV{HOME}/qnx700/host/linux/x86_64/usr/bin/arm-unknown-nto-qnx7.0.0eabi-ar
     CACHE PATH "QNX qr Program" FORCE)
 
 # specify the target environment
-SET(CMAKE_FIND_ROOT_PATH  ${QNX_TARGET}/armle-v7)
+SET(CMAKE_FIND_ROOT_PATH  $ENV{HOME}/qnx700/target/qnx7/armle-v7)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
